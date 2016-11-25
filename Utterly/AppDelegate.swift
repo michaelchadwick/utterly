412bea2f476aa5780751fe8bf43e24b0ce210350
Michael Chadwick <michael.chadwick@gmail.com>
2016-11-25 08:40:05 -0800

    Initial Commit

---
+++ Utterly/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Utterly
+//
+//  Created by Michael Chadwick on 11/25/16.
+//  Copyright © 2016 Michael Chadwick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Utterly/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Utterly
+//
+//  Created by Michael Chadwick on 11/25/16.
+//  Copyright © 2016 Michael Chadwick. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
